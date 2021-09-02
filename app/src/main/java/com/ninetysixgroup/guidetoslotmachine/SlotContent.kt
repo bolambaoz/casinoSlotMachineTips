6c054055ee8112b625398c892d1d52ff6ddb4da2
bolamboaz <bolamboaz@gmail.com>
2021-09-02 17:55:17 +0800

    update slot screen and create new slot detail screen

--- app/src/main/java/com/ninetysixgroup/guidetoslotmachine/SlotContent.kt
+++ app/src/main/java/com/ninetysixgroup/guidetoslotmachine/SlotContent.kt
@@ -2,10 +2,22 @@ package com.ninetysixgroup.guidetoslotmachine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.toolbar.*
 
 class SlotContent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slot_content)
+
+        setSupportActionBar(toolbar)
+
+        toolbar.title = ""
+
+        img_back_arrow_content.setOnClickListener {
+            onBackPressed()
+        }
+
+//        toolbar.isVisible = false
     }
 }
